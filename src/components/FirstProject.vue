98a38c477d452deb78473cbfbd7f0c35385a1413
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2018-05-22 17:37:53 -0600

    Finished implementing component functionality to create a project.

--- src/components/FirstProject.vue
+++ src/components/FirstProject.vue
@@ -6,17 +6,26 @@
             <p class="lead text-center w-75 mx-auto">Project time trackers allows you to create projects, assign categories, and keep track of all your time spent.</p>
             <input type="text" name="projectName" id="projectName" class="form-control w-50 mx-auto" v-model="projectName">
             <div class="d-flex justify-content-center align-content-center w-100">
-                <button class="text-center btn btn-success btn-lg my-3" v-on:click.prevent="saveProject">Create your first project</button>
+                <button class="text-center btn btn-success btn-lg my-3" v-on:click.prevent="createProject">Create your first project</button>
             </div>
           </div>
     </section>
 </template>
 
 <script>
+import { ErrorsBus } from '../main'
+
     export default {
         data: function() {
             return {
                 projectName: ''
+            }
+        },
+        methods: {
+            createProject: function() {
+                this.$store.dispatch('createProject', this.projectName)
+                    .then(res => this.$router.push('processing'))
+                    .catch(err => ErrorsBus.errorHandler(err))
             }
         }
     }
