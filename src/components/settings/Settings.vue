d3753657337bd8ea5e44832caca1a6d060477344
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2018-05-24 11:10:55 -0600

    Removed unused commented out code and added modal to settings page for purposes of adding a category.

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -8,39 +8,13 @@
         <!-- PROJECT EDIT BODY -->
         <div class="container-fluid pt-3" v-for="(project, key, index) in projects" :key="index">
             <app-project :project="project"></app-project>
-                    <!-- <div class="row py-2 mx-4" >
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                    <input
-                        class="h4 w-100"
-                        type="text"
-                        :value="project.project_name"
-                        :class="{
-                        'border border-primary bg-light': !isDisabled,
-                        'bg-transparent border-0': isDisabled,
-                        }"
-                        v-bind:disabled="isDisabled">
-                </div>
-                <div class="col-lg-6 col-sm-4">
-                    <button
-                        class="btn btn-primary btn-sm mx-1"
-                        v-on:click="onEdit()"
-                        v-if="isDisabled">Edit</button>
-                    <button
-                        class="btn btn-warning btn-sm mx-1"
-                        v-else
-                        v-on:click="onSave()">Save</button>
-                    <button
-                        class="btn btn-danger btn-sm mx-1"
-                        v-on:click="deleteProject(project.project_id)">Delete</button>
-                </div>
-            </div> -->
         </div>
 
         <!-- CATEGORY HEADER -->
         <div class="container-fluid mt-4 bg-light border">
             <div class="row justify-content-between align-items-center mx-3">
                 <p class="display-4">Categories</p>
-                <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addCategory">Add Category</button>
+                <button class="btn btn-primary btn-sm" @click="display = true">Add Category</button>
             </div>
         </div>
 
@@ -48,6 +22,31 @@
         <div class="container-fluid pt-4" v-for="(category, key, index) in projectCategories" :key="index" v-if="category.hidden != 1 && key !== 0 ">
             <app-category :category="category" :index="index" ></app-category>
         </div>
+
+
+        <!-- NEW PROJECT MODAL -->
+        <b-modal
+            class="modal-title"
+            title="Add a New Category"
+            role="dialog"
+            v-model="display"
+            >
+            <p>Enter a name for your new category</p>
+            <form id="newCategoryForm">
+                <div class="form-group">
+                    <label for="categoryName">Category Name</label>
+                    <input type="text" name="category_name" id="category_name" class="form-control" required v-model="categoryName">
+                    <div class="invalid-feedback">
+                        Please provide a category name.
+                    </div>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button type="button" class="btn btn-secondary mx-1" @click="display = false">Close</button>
+                    <button type="submit" class="btn btn-primary mx-1" v-on:click.prevent="addCategory">Save changes</button>
+                </div>
+            </form>
+            <div slot="modal-footer"></div>
+        </b-modal>
     </div>
 </template>
 
@@ -59,7 +58,8 @@ import SettingsCategoryItem from './SettingsCategoryItem.vue'
     export default {
         data: function() {
             return {
-                editedName: ""
+                categoryName: "",
+                display: false
             }
         },
         components: {
@@ -74,6 +74,15 @@ import SettingsCategoryItem from './SettingsCategoryItem.vue'
                 return this.$store.getters.projectCategories;
             }
         },
+        methods: {
+            addCategory: function() {
+                this.$store.dispatch('addCategory', this.categoryName)
+                    .then(res => this.display = false)
+                    .catch(err => {
+                        ErrorsBus.errorHandler(err);
+                    })
+            }
+        }
     }
 </script>
 
