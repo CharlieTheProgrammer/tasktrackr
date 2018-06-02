e8ce86e50de686dc32e0d04ebdf52be03125d47d
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2018-06-02 13:04:06 -0500

    Fixed HTML issue with divs.

--- src/components/settings/SettingsCategoryItem.vue
+++ src/components/settings/SettingsCategoryItem.vue
@@ -2,30 +2,28 @@
     <div>
         <div class="row py-2 mx-4" >
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                <div>
-                    <input
-                        class="h4 w-100"
-                        type="text"
-                        v-model="name"
-                        :class="{
-                            'border border-primary bg-light': !isDisabled,
-                            'bg-transparent border-0': isDisabled,
-                            }"
-                        :disabled="isDisabled">
-                </div>
-                <div class="col-lg-6 col-sm-4">
-                    <button
-                        class="btn btn-primary btn-sm mx-1"
-                        v-on:click="onEdit()"
-                        v-if="isDisabled">Edit</button>
-                    <button
-                        class="btn btn-warning btn-sm mx-1"
-                        v-else
-                        v-on:click="onSave()">Save</button>
-                    <button
-                        class="btn btn-danger btn-sm mx-1"
-                        v-on:click="onDelete()">Delete</button>
-                </div>
+                <input
+                    class="h4 w-100"
+                    type="text"
+                    v-model="name"
+                    :class="{
+                        'border border-primary bg-light': !isDisabled,
+                        'bg-transparent border-0': isDisabled,
+                        }"
+                    :disabled="isDisabled">
+            </div>
+            <div class="col-lg-6 col-sm-4">
+                <button
+                    class="btn btn-primary btn-sm mx-1"
+                    v-on:click="onEdit()"
+                    v-if="isDisabled">Edit</button>
+                <button
+                    class="btn btn-warning btn-sm mx-1"
+                    v-else
+                    v-on:click="onSave()">Save</button>
+                <button
+                    class="btn btn-danger btn-sm mx-1"
+                    v-on:click="onDelete()">Delete</button>
             </div>
         </div>
     </div>
