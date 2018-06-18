7787aee6bd5813f8ff51e1f994fca0cd3ab004d5
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2018-06-18 17:45:55 -0500

    Implemented check on deleted categories so that they only appear if previously selected for existing entry.
    Deleted category will disappear from dropdown if user picks another option.

--- src/components/project/Entry.vue
+++ src/components/project/Entry.vue
@@ -1,42 +1,41 @@
 <template>
-
-        <tr>
-            <td>
-                <select
-                    class="custom-select w-100"
-                    :data-entry_id="entry.entry_id"
-                    data-cat="category_id"
-                    >
-                    <option
-                        v-for="(categoryItem, index) in projectCategories" :key="index"
-                        :selected="entry.category_id == categoryItem.category_id ? 'selected' : null"
-                        :value="categoryItem.category_id"
-                        >{{ categoryItem.category_name}}</option>
-                </select>
-            </td>
-            <td
-                class="text-center"
+    <tr>
+        <td>
+            <select
+                class="custom-select w-100"
                 :data-entry_id="entry.entry_id"
-                data-cat="entry_date">{{ entry.entry_date }}</td>
-            <td>
-                <textarea
-                    class="w-100 bg-transparent"
-                    :data-entry_id="entry.entry_id"
-                    data-cat="entry_description"
-                    :value="entry.entry_description"
-                    >
-                </textarea>
-            </td>
-            <td
-                class="text-center"
-                :data-entry_id="entry.entry_id" data-cat="start_time">{{ entry.start_time | toTimeFormat }}
-            </td>
-            <td
-                class="text-center"
-                :data-entry_id="entry.entry_id">{{ entry.total_time | generateTotalTime(entry.start_time) }}
-            </td>
-        </tr>
-
+                data-cat="category_id"
+                >
+                <option
+                    v-for="(categoryItem, index) in projectCategories" :key="index"
+                    :selected="entry.category_id == categoryItem.category_id ? 'selected' : null"
+                    :value="categoryItem.category_id"
+                    v-if="show(entry, categoryItem)"
+                    >{{ categoryItem.category_name}}</option>
+            </select>
+        </td>
+        <td
+            class="text-center"
+            :data-entry_id="entry.entry_id"
+            data-cat="entry_date">{{ entry.entry_date }}</td>
+        <td>
+            <textarea
+                class="w-100 bg-transparent"
+                :data-entry_id="entry.entry_id"
+                data-cat="entry_description"
+                :value="entry.entry_description"
+                >
+            </textarea>
+        </td>
+        <td
+            class="text-center"
+            :data-entry_id="entry.entry_id" data-cat="start_time">{{ entry.start_time | toTimeFormat }}
+        </td>
+        <td
+            class="text-center"
+            :data-entry_id="entry.entry_id">{{ entry.total_time | generateTotalTime(entry.start_time) }}
+        </td>
+    </tr>
 </template>
 
 <script>
@@ -53,9 +52,22 @@
                 required: true
             }
         },
-        data: function() {
-            return {
-                tt: ""
+        methods: {
+            show: function(entry, category) {
+                // Hidden/deleted category should only appear for previous entries that had that category
+                // Else, do not show hidden category in dropdown and make it disappear if user selects another option
+
+                // category hidden	category selected	Action
+                // TRUE	            TRUE	            Display
+                // TRUE	            FALSE	            Do not display
+                // FALSE	        TRUE	            Display
+                // FALSE	        FALSE	            Display
+
+                if (category.hidden === 1 && !(entry.category_id == category.category_id)) {
+                    return false;
+                } else {
+                    return true;
+                }
             }
         },
         watch: {
